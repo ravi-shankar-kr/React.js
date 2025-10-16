8362d32cc4579c39b52dae1e9a6f0f2ec93e096a
Ravi_Shankar_kr <ravi896959@gmail.com>
2025-10-16 20:15:51 +0530

    first commit

---
+++ 02-component/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './components/Card'
+import AboutCard from './components/AboutCard'
+
+const App = () => {
+  return (
+    <div> 
+      <AboutCard/>
+      {/* <Card/> */}
+    </div>
+  )
+}
+
+export default App
---
+++ 02-component/src/components/AboutCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutCard = () => {
+  return (
+    <div>
+        <div className='card'>
+            <h1>Hi I'm Ravi</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, deleniti!</p>
+        </div>
+    </div>
+  )
+}
+
+export default AboutCard;
