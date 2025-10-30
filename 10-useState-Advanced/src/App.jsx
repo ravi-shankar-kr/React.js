bef33e3cdf6513cb03205ac9f920c29e590f9829
Ravi_Shankar_kr <ravi896959@gmail.com>
2025-10-30 20:18:55 +0530

    practice advanced useState

--- 10-useState-Advanced/src/App.jsx
+++ 10-useState-Advanced/src/App.jsx
@@ -6,12 +6,56 @@ const App = () => {
 
   const btnClick= () => {
     setnum (20);
+     
   }
+
+
+  const [user, setUser] = useState({user:'Ravi', age:22});  //Arrays
+
+  const btnisClick= () => {
+     const newuser = {...user};
+    //  console.log(newuser);
+    newuser.user = 'Muskan'
+    newuser.age = 24
+
+    setUser(newuser) 
+  }
+
+
+
+  const [arr, setArr] = useState([10, 20, 30]);  //Arrays
+
+  const addArray= () => {
+     const newarr = [...arr];
+     newarr.push(90);
+     setArr(newarr);
+     
+    //  console.log(arr);
+  }
+
+  const [useArr, setUserArr] = useState({user:'Ravi', age:22});  //Arrays
+  const userArray= () => {
+    setUserArr(prev => ({...prev, age:50}))
+   console.log(useArr);
+  }
+  
+
+
 
   return (
     <div>
       <h1 className='text-5xl m-6  '>{num}</h1>
       <button onClick={btnClick} className='px-5 py-3 m-3 rounded-3xl bg-gray-200'>Click</button>
+
+      <h1 className='text-5xl m-6  '>{user.user}, {user.age}</h1>
+      <button onClick={btnisClick} className='px-5 py-3 m-3 rounded-3xl bg-gray-200'>Click</button>
+
+      <h1 className='text-5xl m-6  '>{arr}</h1>
+      <button onClick={addArray} className='px-5 py-3 m-3 rounded-3xl bg-gray-200'>Click</button>
+
+      <h1 className='text-5xl m-6  '>{useArr.user}, {useArr.age}</h1>
+      <button onClick={userArray} className='px-5 py-3 m-3 rounded-3xl bg-gray-200'>Click</button>
+    
     </div>
   )
 }
