20a26d1c44f0d26523e5735de39ded4ecd3e8ef9
Ravi_Shankar_kr <ravi896959@gmail.com>
2025-11-08 23:09:43 +0530

    learn useEffect

---
+++ 16-useEffect/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+const App = () => {
+  const [num, setNum] = useState(0)
+  const [num2, setNum2] = useState(110)
+ 
+  useEffect(function() {
+    console.log("useeffect is running");
+    
+  },[num])
+
+  return (
+    <div>
+      <h1 className='font-bold'> num: {num}</h1>
+      <h1 className='font-bold'>num2: {num2}</h1>
+
+      <button onMouseEnter={() => {
+        setNum(num+1)
+      }}
+      onMouseLeave={() => {
+        setNum2(num2+10)
+      }}
+      className='px-5 py-2 border bg-gray-400 '
+      >click</button>
+    </div>
+  )
+}
+
+export default App
