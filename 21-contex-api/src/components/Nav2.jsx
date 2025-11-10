d048ae7541f93d6f37368c850aea3708f1b1225d
Ravi_Shankar_kr <ravi896959@gmail.com>
2025-11-10 23:48:08 +0530

    first commit

---
+++ 21-contex-api/src/components/Nav2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Nav2 = () => {
+  return (
+    <div>
+        <h4>Home</h4>
+        <h4>About</h4>
+        <h4>Contact</h4>
+        <h4>Services</h4>
+    </div>
+  )
+}
+
+export default Nav2
